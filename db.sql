82f32096e0f0929088f2ec296ce73f43765696a6
gsvsenai <gabrielvieira.senai@gmail.com>
2026-09-22 13:54:43 -0300

    UPDATE DATABASE

--- db.sql
+++ db.sql
@@ -1,1 +1,2 @@
-CREATE TABLE IF NOT EXISTS public."Ferramentas" (id bigint NOT NULL, status boolean, last_update timestamp with time zone DEFAULT now(), last_user bigint, nome character varying NOT NULL);
+CREATE TABLE IF NOT EXISTS public."Ferramentas" (id bigint NOT NULL, last_user bigint, nome character varying NOT NULL, status boolean, last_update timestamp with time zone DEFAULT now());
+CREATE TABLE IF NOT EXISTS public.esp32_status (last_ping timestamp with time zone DEFAULT now(), id bigint NOT NULL);
